5a319e2a3ba90a443934373d3fb605f65d7928be
SulaimaanS <Sulaimaansharif@y7mail.com>
2020-10-13 18:00:10 +1100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -16,46 +16,57 @@ namespace AD.NETA2 { //TODO: Login function to check for multiple accounts that 
             InitializeComponent();
         }
 
-        private string username, password;
+        private string UserInput, PassInput;
         NewAccountWindow AccountForm = new NewAccountWindow();
         TextEditorWindow TextEditor = new TextEditorWindow();
 
         private void storeUsers() {
-            string[] Users = File.ReadAllLines(@"login.txt");
             //Use this to store users in an array and then feed that into login and split for index 0 and 1
 
         }
 
         private void Login() {
-            username = LoginUserField.Text;
-            password = LoginPasswordField.Text;
-            string[] Logins = File.ReadAllText(@"login.txt").Split(',');
-            if (username == Logins[0] && password == Logins[1]) {
-                MessageBox.Show("Login Successful!", "Successful Login");
-                TextEditor.Show();
-            }
-            else {
-                DialogResult YesOrNo = MessageBox.Show("We couldn't find an account with these credentials!\nWould you like to create an account?", "Failed Login", MessageBoxButtons.YesNo);
-                if (YesOrNo == DialogResult.Yes) {
-                    if (AccountForm.IsDisposed) {
-                        NewAccountWindow AccountForm = new NewAccountWindow();
-                        AccountForm.Show();
-                    }
-                    else {
-                        AccountForm.Show();
-                    }
+            bool LoginComplete = false;
+            string[] Users = File.ReadAllLines(@"login.txt"); //Store all user details in an array
+            UserInput = LoginUserField.Text;
+            PassInput = LoginPasswordField.Text;
+            while (!LoginComplete) {
+
+                for (int loop = 0; loop < Users.Length; loop++) {
+
+                    string[] Login = new string[];
+                    Users[loop].Split(Login,StringSplitOptions.None);
                 }
-                
+                if (UserInput == Login[0] && PassInput == Login[1]) {
+
+                    MessageBox.Show("Login Successful!", "Successful Login");
+                    TextEditor.Show();
+                    LoginComplete = true;
+                }
             }
+
+            DialogResult YesOrNo = MessageBox.Show("We couldn't find an account with these credentials!\nWould you like to create an account?", "Failed Login", MessageBoxButtons.YesNo);
+            if (YesOrNo == DialogResult.Yes) {
+                if (AccountForm.IsDisposed) {
+                    NewAccountWindow AccountForm = new NewAccountWindow();
+                    AccountForm.Show();
+                }
+                else {
+                    AccountForm.Show();
+                }
+            }
+
         }
+
+
 
         private void LoginForm_Load(object sender, EventArgs e) {
 
         }
 
         private void LoginButton_Click(object sender, EventArgs e) {
-            //Login();
-            storeUsers();
+            Login();
+            //storeUsers();
         }
 
         private void NewAccountButton_Click(object sender, EventArgs e) {
